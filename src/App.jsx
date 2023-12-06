6232548bf488b8ba399a309263eae318ed1a007d
dumindad <dumindad@gmail.com>
2023-12-06 15:47:28 +0530

    edit navbar

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import NavBar from './components/NavBar'
 import History from './pages/History'
+import Team from './pages/Team'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +13,7 @@ function App() {
     <>
      <NavBar />
      <History />
+     <Team />
     </>
   )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,27 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <nav className={`flex justify-between items-center w-full h-20 px-4`}>
+
+            <div>
+                <span className='text-2xl font-[Poppins] cursor-pointer ml-10'>
+
+                    Logo
+
+                </span>
+
+            </div>
+
+         
+            <div className='w-full'>
+            {/* <MobilMenu
+                    showCatMenu={showCatMenu}
+                    setShowCatMenu={setShowCatMenu}
+                    setMobileMenu={setMobileMenu}
+                /> */}
+            </div>
+
+        </nav>
   )
 }
 
